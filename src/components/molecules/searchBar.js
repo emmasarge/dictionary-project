f41e108e9a32b279be32e91cf5abf3f7abd4659d
Emma <70288571+emmasarge@users.noreply.github.com>
2022-07-01 10:27:56 +0100

    initialise search form

--- src/components/molecules/searchBar.js
+++ src/components/molecules/searchBar.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchBar() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`looking for definition of ${keyword}...`)
+  }
+
+  function handleKeywordChange(event) {
+   setKeyword(event.target.value);
+  }
   return (
     <>
       <div className="text-center mt-3">
-        <h1 >Dictionary</h1>
+        <h1>Dictionary</h1>
         <p>noun</p>
       </div>
       <div>
-          <form className="mx-auto text-center">
-              <input type="text" value="look up..."/>
-              <input type="submit" value="search"/>
-          </form>
+        <form className="mx-auto text-center" onSubmit={search}>
+          <input
+            type="search"
+            placeholder="look up..."
+            onChange={handleKeywordChange}
+          />
+          <input type="submit" value="search" />
+        </form>
       </div>
     </>
   );
