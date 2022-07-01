b76e0aa29ef1642902d8d7b310cf860873b42a22
Emma <70288571+emmasarge@users.noreply.github.com>
2022-07-01 14:11:48 +0100

    integrate dictionary api

--- src/components/molecules/searchBar.js
+++ src/components/molecules/searchBar.js
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
+import axios from 'axios';
+
+
 
 export default function SearchBar() {
   let [keyword, setKeyword] = useState("");
+  
+  function handleResponse(response){
+      let word = response.data[0].word;
+      let meaning = response.data[0].meanings[0].definitions[0].definition;
+      let phonetic = response.data[0].phonetic;
+      let synonym = response.data[0].meanings[0].synonyms;
+      console.log(synonym);
+}
 
   function search(event) {
     event.preventDefault();
     alert(`looking for definition of ${keyword}...`)
+    let apilUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apilUrl).then(handleResponse)
   }
 
   function handleKeywordChange(event) {
    setKeyword(event.target.value);
   }
+
+
   return (
     <>
       <div className="text-center mt-3">
