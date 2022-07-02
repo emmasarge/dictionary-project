5cfee39326b0f2138991c73907f67bb9a2f0b8f0
Emma <70288571+emmasarge@users.noreply.github.com>
2022-07-02 14:28:50 +0100

    eslint fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App container mb-4 maxWidth">
       <SearchBar defaultKeyword={'rose'} />
-      <p className="text-center smallText mt-2 resource-link">This is an open source project by Emma Sargeant available on <a href="https://github.com/emmasarge/dictionary-project" target="_blank"rel="noreferror" >Github</a> and hosted on <a href="https://bespoke-buttercream-ec36b1.netlify.app/" target="_blank" rel="noreferror">Netlify</a></p>
+      <p className="text-center smallText mt-2 resource-link">This is an open source project by Emma Sargeant available on <a href="https://github.com/emmasarge/dictionary-project" target="_blank"ref="noreferror" >Github</a> and hosted on <a href="https://bespoke-buttercream-ec36b1.netlify.app/" target="_blank" ref="noreferror">Netlify</a></p>
   
     </div>
   );
