cd7ea3c8eecb6a3e313fc79f172f44bf170ad6ee
Emma <70288571+emmasarge@users.noreply.github.com>
2022-07-02 10:22:45 +0100

    add mp3 package for audio phonetics

--- src/components/molecules/Phonetics.js
+++ src/components/molecules/Phonetics.js
@@ -1,14 +1,15 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
   return (
     <div className="container border mt-3">
-        <h3>Phonetics</h3>
-      {props.phonetic.phonetics.slice(0,2).map(function (phonetic, index) {
+      <h3>Phonetics</h3>
+      {props.phonetic.phonetics.slice(0, 2).map(function (phonetic, index) {
         return (
           <div key={index}>
             <p>{phonetic.text}</p>
-            <p>{phonetic.audio}</p>
+            <ReactAudioPlayer src={phonetic.audio} onPlay controls />
           </div>
         );
       })}
